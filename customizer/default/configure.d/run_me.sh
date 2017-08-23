ee34c7a7d68629559c5134bf11352fe957355414
Mike Croucher <michael.p.croucher@googlemail.com>
2017-08-23 14:05:39 +0100

    customisation

--- customizer/default/configure.d/run_me.sh
+++ customizer/default/configure.d/run_me.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
-echo "confifure.d"
-echo "$@"
-sudo mkdir /opt/testing
+echo "configure.d script"
+number_of_users=35
+
+#Install mkpasswd
+sudo yum install expect -y
+
+if [ $2 = 'master' ]; then
+ echo "Running on master node"
+ #Master node
+ password_file=/home/walkingrandomly/users.txt
+ touch $password_file
+ for i in `seq 1 $number_of_users`;
+  do
+  username=training_user$i
+  sudo useradd -m -d /home/$username $username
+  userpassword=`mkpasswd`
+  echo $username:$userpassword | sudo chpasswd
+  echo "UserID:" $username "has been created with the following password " $userpassword >> $password_file
+ done
+
+#allow password authentication to the master node
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo service sshd restart
+else
+ #Compute node
+ echo "running on compute node"
+ for i in `seq 1 $number_of_users`;
+ do
+  username=training_user$i
+  sudo useradd -m -d /home/$username $username
+ done
+fi
+
