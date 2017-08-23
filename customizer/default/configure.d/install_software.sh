5e1608cae4cb27e42a9bdae6a6179fea36ae339b
Mike Croucher <michael.p.croucher@googlemail.com>
2017-08-23 18:23:11 +0100

    Install software

---
+++ customizer/default/configure.d/install_software.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /etc/profile.d/alces-clusterware.sh
+echo 'Install software'
+
+alces gridware install main/mpi/mpich2/1.5
+alces gridware install main/mpi/openmpi/1.10.2
+alces gridware install main/apps/R/3.4.0
